fda07828b207d714e3a015eee9f6340ff542e377
Rabee <Rabeeekiblawi@gmail.com>
2019-05-21 10:32:41 +0300

    added safari plugin

--- src/platform/cordova.ts
+++ src/platform/cordova.ts
@@ -1,53 +1,53 @@
 import { Oauth } from '../oauth';
-
+import {SafariViewController} from '@ionic-native/safari-view-controller'
 declare var window: any;
 
 function ensureEnvIsValid() {
   if (!window.cordova) {
       throw new Error('Cannot authenticate via a web browser');
   }
-
-  if (!window.cordova.InAppBrowser) {
-      throw new Error('The Apache Cordova InAppBrowser plugin was not found and is required');
-  }
 }
+function openUrl(URL: string)
+{
+  SafariViewController.isAvailable()
+      .then((available: boolean) => {
+        if (available) {
+          this.safariViewController.show({
+            url: URL,
+            hidden: false,
+            animated: false,
+            transition: 'curl',
+            enterReaderModeIfAvailable: false,
+            tintColor: '#ff0000'
+          })
+            .subscribe((result: any) => {
+              if (result.event === 'opened') {
+                console.log('Opened');
+              } else if (result.event === 'loaded') {
+                console.log('Loaded');
+              } else if (result.event === 'closed') {
+                console.log('Closed');
+              }
+            },
+              (error: any) => console.error(error)
+            );
 
+        } else {
+          // use fallback browser, example InAppBrowser
+        }
+      }
+      );
+}
 export class OauthCordova extends Oauth {
   defaultWindowOptions = {
       location: 'no',
       clearsessioncache: 'yes',
       clearcache: 'yes'
   };
-
   protected openDialog(url: string, windowParams: Object, options: any = {}) {
     const params = this.serializeOptions(windowParams);
-
     return new Promise((resolve, reject) => {
-        try {
-          ensureEnvIsValid();
-        } catch (error) {
-          return reject(error);
-        }
-
-        const browserRef = window.cordova.InAppBrowser.open(url, '_blank', params);
-        const exitListener = () => reject(new Error(`The "${options.providerName}" sign in flow was canceled`));
-
-        browserRef.addEventListener('loaderror', (error) => {
-          if (error.code == "-999") return; 
-          browserRef.removeEventListener('exit', exitListener);
-          browserRef.close();
-          reject(new Error(`Error loading login page of "${options.providerName}"`));
-        });
-
-        browserRef.addEventListener('loadstart', (event) => {
-            if (event.url.indexOf(options.resolveOnUri) === 0) {
-                browserRef.removeEventListener('exit', exitListener);
-                browserRef.close();
-                resolve(event)
-            }
-        })
-
-        return browserRef.addEventListener('exit', exitListener);
+        openUrl(url);
     })
   }
 }
